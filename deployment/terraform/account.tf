548d7e0a32f1c9260aac9f5925e0c2b393adb872
Farkhod <sadykovfarkhod@gmail.com>
2019-03-31 21:33:15 -0500

    Added location of the service account

--- deployment/terraform/account.tf
+++ deployment/terraform/account.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("/root/.kube/flask-kube.json")}"
+  credentials = "${file("/fuchicorp/flask-kube.json")}"
   project     = "striking-gadget-234002"
   region      = "us-central1-a"
 }
