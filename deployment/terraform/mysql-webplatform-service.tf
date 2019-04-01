913fa634bb02eb904d356fc39bd9db7a0145bc04
Farkhod <sadykovfarkhod@gmail.com>
2019-03-31 19:26:23 -0500

    Deleted sluster ip

--- deployment/terraform/mysql-webplatform-service.tf
+++ deployment/terraform/mysql-webplatform-service.tf
@@ -4,13 +4,11 @@ resource "kubernetes_service" "mysql_host" {
     namespace = "${var.webplatform_namespace}"
   }
   spec {
-    clusterIP = "null"
+    cluster_ip = "null"
     selector { run = "webplatform-mysql"  }
     port {
       port = 3306
       target_port = 3306
     }
-
-    type = "ClusterIP"
   }
 }
